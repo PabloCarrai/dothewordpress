1e5c0dc7da0b2e3e4a2df4e1d097689fb9e26bf9
PabloCarrai <pablohernanjuan@hotmail.com>
2023-04-23 02:39:24 +0000

    Agregando una ayuda

--- dothewordpressturururu.sh
+++ dothewordpressturururu.sh
@@ -15,13 +15,33 @@ OK="
 ⣿⣿⣧⣀⣿………⣀⣰⣏⣘⣆⣀⠀⠀
 "
 
-
-
 DIR="$PWD"
 BA=$(which bash)
 FI=$(which find)
 ARCHIVO=$($FI "$DIR" -type f -name info)
 PLUG="$DIR"/"$1"
+
+help()
+{
+   echo "Script de creacion de sitio en docker-compose"
+   echo
+   echo "Uso: ./script url"
+   echo "Es decir seria mas o menos asi"
+   echo "     ./dothewordpressturururu.sh sitio.org"
+
+}
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         help
+         exit;;
+      *) # incorrect option
+         echo "Error: Opcion invalida"
+	 help
+         exit;;
+   esac
+done
 
 git clone https://github.com/PabloCarrai/wp-dockerizado.git &> /dev/null
 mv "$DIR"/wp-dockerizado "$DIR"/"$1"
